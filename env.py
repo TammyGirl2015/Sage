7ab6aa0cefdc5e6fa6be1d79d4dfc0c18804c4ee
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-22 16:18:03 +0000

    add procfile, media, templates and static file

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"]='postgresql://neondb_owner:ZidTX3WeLOV1@ep-delicate-queen-a29jigzg.eu-central-1.aws.neon.tech/neondb?sslmode=require'
 os.environ["SECRET_KEY"]='django-insecure-baqmhqts-2=5kdsu9mzg4jgsz%s)*+qz=!x#uf3f45764!gu-w'
+os.environ["CLOUDINARY_URL"]= 'cloudinary://<your_api_key>:<your_api_secret>@dnycijj5z'
