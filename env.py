e250b9357152fd1fff02bad188a843aa4d13209a
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-22 15:44:30 +0000

    install django libraries

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"]='postgresql://neondb_owner:ZidTX3WeLOV1@ep-delicate-queen-a29jigzg.eu-central-1.aws.neon.tech/neondb?sslmode=require'
+os.environ["SECRET_KEY"]='django-insecure-baqmhqts-2=5kdsu9mzg4jgsz%s)*+qz=!x#uf3f45764!gu-w'
---
+++ sageadvice/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SageadviceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sageadvice'
