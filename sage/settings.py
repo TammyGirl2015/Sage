c6c8447a8f537e7652164a7587eec78ab9bf08bb
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-22 16:40:30 +0000

    add procfile

--- sage/settings.py
+++ sage/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sage-advice.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sage.herokuapp.com', 'localhost', '8000-tammygirl2015-sage-znmr2ztblji.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -133,12 +133,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage',
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
