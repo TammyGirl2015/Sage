a32ec446d3ab5b5e1331ce266db2533e008b458d
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-10-01 22:15:21 +0000

    deploy to heroku

--- sage/settings.py
+++ sage/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
-
 import os
+from pathlib import Path
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -29,10 +28,14 @@ TEMPLATES_DIR =  os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['sage.herokuapp.com', 'localhost', '8000-tammygirl2015-sage-znmr2ztblji.ws.codeinstitute-ide.net']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-tammygirl2015-sage-znmr2ztblji.ws.codeinstitute-ide.net',
+]
 
 # Application definition
 
@@ -42,12 +45,34 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'sageadvice',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'sage.urls'
@@ -146,3 +172,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- sage/urls.py
+++ sage/urls.py
@@ -13,20 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
-#from django.urls import path, include
-
-#urlpatterns = [
-#    path('admin/', admin.site.urls),
-#    path('summernote/', include('django_summernote.urls')),
-#    path('blog/', include('sage.urls'), name='sage_urls'),
-#]
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("sageadvice.urls"), name="sage_urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("", include("sageadvice.urls"), name="sage_urls"),
+    path('accounts/', include('allauth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
